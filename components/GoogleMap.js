1a518ade54bab794da40d00b3a4060efb61c1875
Paul McKenna <user1@user1s-mbp.home>
2019-10-14 11:36:58 +0100

    edited home page and areas

--- components/GoogleMap.js
+++ components/GoogleMap.js
@@ -5,7 +5,7 @@ const AnyReactComponent = ({ text }) => {
     return (
         <div style={{maxWidth: '10px'}}>            
             <figure className="image is-96x96">
-                <img src="/static/images/logo.jpg" />
+                <img src="/static/images/logo-x.png" />
             </figure>
         </div>
         
@@ -27,7 +27,7 @@ class SimpleMap extends Component {
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: '' }}
+          bootstrapURLKeys={{ key: 'AIzaSyBSbNGSoHm2Q9fve8ltO7COoAjZLPE5Fj0' }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
--- components/Home.js
+++ components/Home.js
@@ -46,49 +46,81 @@ const Home = () => {
     return (
         <div>
             {/* Header */}
-            <div style={contentStyle}>
-                <div style={{ display: 'flex', alignItems: 'center', minHeight: '80vh'}}>
-                    <div className='container' style={{padding: '1rem'}}>
-                        <div className='has-text-centered' style={{background: 'rgba(16,46,16, 0.7)', padding: '3rem 0'}}>
-                            <FadeIn transitionDuration={2500}>
-                            <h1 className="title is-3" style={{color: '#FCFAFA'}}><span className='is-hidden-mobile'>Established And Trustworthy</span> Commercial Garden &amp; Grounds Maintenance</h1>
-                            <h2 className="subtitle is-3" style={{color: '#FCFAFA', padding: '2rem 1rem 0 1rem', fontSize: '1.1rem', lineHeight: '1.95rem'}}>Welcome to Greenscapes Commercial Garden and Grounds Services. </h2>
-                            <p className="subtitle is-3" style={{color: '#FCFAFA', padding: '1rem 1rem 0 1rem', fontSize: '1.1rem', lineHeight: '1.95rem'}}>We provide professional garden &amp; grounds maintenance in Cheshire, South Manchester and surrounding areas.</p>
-                            <h3 className='subtitle is-3' style={{color: '#FCFAFA', padding: '1rem 1rem 2rem 1rem', fontSize: '1.1rem', lineHeight: '1.95rem'}}>We can help you to promote your corporate image by having well maintained gardens and grounds.</h3>
-                            <div>
-                                <Link href='/contact'>
-                                    <button className={`button is-primary is-medium`}>
-                                        <a style={{color: 'white'}}>
-                                            <span>
-                                                <i style={{marginRight: '0.5rem'}} className='fa fa-envelope'>
-                                                </i>
-                                            </span>
-                                            Contact Us
-                                        </a>
-                                    </button>
-                                </Link>
-                                <span style={{marginLeft: '1rem'}}>
-                                    <button className={`button is-primary is-outlined is-medium is-hidden-mobile`}>
-                                        <a style={{color: 'white'}} href='tel:07772 656034'>
-                                            <span>
-                                                <i style={{marginRight: '0.5rem'}} className='fa fa-phone'>
-                                                </i>
-                                            </span>
-                                            Call Us
-                                        </a>
-                                    </button>
-                                </span>
+            <div className="wave-container">
+                <div className="columns is-centered" style={{ margin: '3rem 0.5rem -1rem 0.5rem' }}>
+                    <div className="column is-half">
+                        <div className='hero is-bold is-info'>
+                            <div className='hero-body'>
+                                <div className='content'>
+                                    <h1><span className='is-hidden-mobile'>Established And Trustworthy</span> Commercial Garden &amp; Grounds Maintenance</h1>
+                                    <div className='columns'>
+                                        <div className='column'>
+                                            <img src="/static/images/logo-x.png" alt="greenscapes logo" style={{ maxWidth: '155px' }} />
+                                        </div>
+                                    </div>
+                                    <p>We provide professional garden &amp; grounds maintenance in Cheshire, South Manchester and the North West.</p>
+                                    <div>
+                                        <Link href='/contact'>
+                                            <button className={`button is-primary is-medium is-hidden-mobile`}>
+                                                <a style={{color: 'white'}}>
+                                                    <span>
+                                                        <i style={{marginRight: '0.5rem'}} className='fa fa-envelope'>
+                                                        </i>
+                                                    </span>
+                                                    Contact Us
+                                                </a>
+                                            </button>
+                                        </Link>
+                                        <span style={{marginLeft: '1rem'}}>
+                                            <button className={`button is-primary is-outlined is-medium`}>
+                                                <a style={{color: 'white'}} href='tel:07772 656034'>
+                                                    <span>
+                                                        <i style={{marginRight: '0.5rem'}} className='fa fa-phone'>
+                                                        </i>
+                                                    </span>
+                                                    Call Us
+                                                </a>
+                                            </button>
+                                        </span>
+                                    </div>
+                                </div>
                             </div>
-                            </FadeIn>
-                        </div>            
+                        </div>
                     </div>
-                </div> 
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 240" display='block'>
+                    <path fill="#1C541D" fillOpacity="1" d="M0,160L60,170.7C120,181,240,203,360,176C480,149,600,75,720,85.3C840,96,960,192,1080,202.7C1200,213,1320,139,1380,101.3L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+                </svg>
+            </div>
+            <div className='wave-container-bottom'>
+                <div className='container' style={{ border: '1px solid #1C541D' }}>
+                    <div className='content'>
+                        <h3><em>"Promote your corporate image by having well maintained gardens and grounds."</em></h3>
+                        <hr/>
+                        <Link href='/services'>
+                            <button className={`button is-primary is-medium`}>
+                                <a style={{color: 'white'}}>
+                                    <span>
+                                        <i style={{marginRight: '0.5rem'}} className='fa fa-cogs'>
+                                        </i>
+                                    </span>
+                                    Find out about our Services
+                                </a>
+                            </button>
+                        </Link>
+                        <br/>
+                        <br/>
+                    </div>
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 240" display='block'>
+                    <path fill="#FCFAFA" fillOpacity="1" d="M0,0L60,16C120,32,240,64,360,90.7C480,117,600,139,720,122.7C840,107,960,53,1080,32C1200,11,1320,21,1380,26.7L1440,32L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+                </svg>
             </div>
             {/* Middle upper */}
             <div className='container' style={{minHeight: '40vh'}}>
                 <div className='columns is-centered' style={{padding: '2rem 1rem 1rem 1rem', margin: '0'}}>
                     <div className='column' > 
-                        <img src="/static/images/garden4.jpeg" alt="" style={{maxHeight: '75%'}}/>
+                        <img src="/static/images/peoples-home.jpeg" alt="" style={{maxHeight: '75%'}}/>
                     </div>
                     <div className='column'>
                         <div className='content'>                            
@@ -100,7 +132,7 @@ const Home = () => {
                                     </div>
                                 </div>
                             </div>                               
-                            <p>We provide professional garden &amp; grounds maintenance in Cheshire, South Manchester and surrounding areas.</p>                            
+                            <p>We provide professional garden &amp; grounds maintenance in Cheshire, South Manchester and the North West</p>                            
                             <h2>Commercial Ground Maintenance</h2>
                             <p>We offer commercial garden and grounds maintenance to a range of
                             services including: </p>
@@ -116,7 +148,31 @@ const Home = () => {
                                 <li>Block Management Companies</li>
                             </ul>
                             <p>Our qualified maintenance teams provide a range of services to ensure that your premises
-                            continue to look their best throughout the year.</p>                            
+                            continue to look their best throughout the year.</p>   
+                            <div>
+                                <Link href='/contact'>
+                                    <button className={`button is-primary is-medium`}>
+                                        <a style={{color: 'white'}}>
+                                            <span>
+                                                <i style={{marginRight: '0.5rem'}} className='fa fa-envelope'>
+                                                </i>
+                                            </span>
+                                            Contact Us
+                                        </a>
+                                    </button>
+                                </Link>
+                                <span style={{marginLeft: '1rem'}}>
+                                    <button className={`button is-primary is-outlined is-medium  is-hidden-mobile`}>
+                                        <a style={{color: 'black'}} href='tel:07772 656034'>
+                                            <span>
+                                                <i style={{marginRight: '0.5rem'}} className='fa fa-phone'>
+                                                </i>
+                                            </span>
+                                            Call Us
+                                        </a>
+                                    </button>
+                                </span>
+                            </div>                         
                         </div>
                     </div>
                 </div>
@@ -143,7 +199,7 @@ const Home = () => {
                     {
                         images.map(image => {
                             return (
-                                <div className="column is-one-fifth" style={{padding: '2rem'}}>
+                                <div className="column is-one-fifth" style={{padding: '2rem'}} key={image}>
                                     <div>
                                         <div style={{maxWidth: '7.5rem', margin: 'auto'}}>
                                             <img src={`/static/images/${image}`} alt={image} />
@@ -156,6 +212,34 @@ const Home = () => {
                 </div>
             </div>
             <Testimonial />
+            <style jsx>{`
+            .content h1 {
+                color: white;
+            }
+            .content h3 {
+                color: white;
+            }
+            .subtitle {
+                color: white;
+            }
+            .wave-container {
+                background-image: url('/static/images/office_garden.jpg');
+                background-repeat: no-repeat;
+                background-position: center center;
+                background-size: cover;
+                position: relative; 
+                color: #FFF; 
+                text-align: center; 
+                overflow: hidden;
+            }
+            .wave-container-bottom {
+                background: #1C541D; 
+                position: relative; 
+                color: #FFF; 
+                text-align: center; 
+                overflow: hidden;
+            }
+        `}</style>
         </div>
     )
 }
